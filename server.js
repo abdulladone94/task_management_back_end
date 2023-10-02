bfbdecdb0b4686a0c6f90a7e8c4e2ff06525a00c
abdulladone94 <abdullaf@booleanlabs.biz>
2023-10-02 11:29:57 +0530

    setup mysql and get all tasks

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
-import express from "express";
+import express, { json } from "express";
+import mysql from "mysql";
+import cors from "cors";
 
 const app = express();
 const port = 8081;
+app.use(express.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "1234",
+  database: "task_management",
+});
 
 app.get("/", (req, res) => {
-  res.json("Hello World");
+  const allTasks = "SELECT * FROM task";
+  db.query(allTasks, (err, data) => {
+    if (err) return res.json("Error");
+    return res.json(data);
+  });
 });
 
 app.listen(port, () => {
